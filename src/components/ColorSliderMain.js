94c0f5af46e21e17f7c7e0a0e63045f59e74e5aa
Will Murchison <wmurch@hotmail.com>
2019-06-03 14:28:09 -0400

    added hook syntex

--- src/components/ColorSliderMain.js
+++ src/components/ColorSliderMain.js
@@ -1,100 +1,90 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 //change background color of page on load
 //randomly generate number for H,S and L
 
-class ColorSliderMain extends Component {
-  state = {
-    hue: 0,
-    saturation: 0,
-    lightness: 0
-  }
-  updateColor = () => {
-    //update hue when page loads
-    const randomHue = Math.ceil(Math.random() * 360)
-    console.log(randomHue)
-    //update saturation when page loads
-    const randomSaturation = Math.ceil(Math.random() * 100)
-    console.log(randomSaturation)
-    //update lightness when page loads
-    const randomLightness = Math.ceil(Math.random() * 100)
-    console.log(randomLightness)
-    this.setState({
-      lightness: randomLightness,
-      hue: randomHue,
-      saturation: randomSaturation
-    })
-  }
-  componentDidMount() {
-    this.updateColor()
-  }
-  updateHue = event => {
-    // const randomHue = Math.ceil(Math.random() * 360)
+export default function ColorSliderMain() {
+  const [hue, setHue] = useState(Math.ceil(Math.random() * 360))
+  const [saturation, setSaturation] = useState(Math.ceil(Math.random() * 100))
+  const [lightness, setLightness] = useState(Math.ceil(Math.random() * 100))
 
-    console.log(event.target.value)
-    this.setState({ hue: event.target.value })
+  const updateColor = () => {
+    //update hue when page loads
+    setHue(Math.ceil(Math.random() * 360))
+    //const randomHue = Math.ceil(Math.random() * 360)
+    console.log(setHue)
+    //update saturation when page loads
+    setSaturation(Math.ceil(Math.random() * 100))
+    //const randomSaturation = Math.ceil(Math.random() * 100)
+    console.log(setSaturation)
+    //update lightness when page loads
+    setLightness(Math.ceil(Math.random() * 100))
+    //const randomLightness = Math.ceil(Math.random() * 100)
+    console.log(setLightness)
   }
-  updateSaturation = event => {
+  const updateHue = event => {
+    // const randomHue = Math.ceil(Math.random() * 360)
+    console.log(event.target.value)
+    setHue(event.target.value)
+    //this.setState({ hue: event.target.value })
+  }
+  const updateSaturation = event => {
+    setSaturation(event.target.value)
     //const randomSaturation = Math.ceil(Math.random() * 100)
     //console.log(randomSaturation)
-    this.setState({ saturation: event.target.value })
+    //this.setState({ saturation: event.target.value })
   }
-  updateLightness = event => {
+  const updateLightness = event => {
+    setLightness(event.target.value)
     // const randomLightness = Math.ceil(Math.random() * 100)
     //console.log(randomLightness)
-    this.setState({ lightness: event.target.value })
+    // this.setState({ lightness: event.target.value })
   }
-  render() {
-    return (
-      <main
-        style={{
-          backgroundColor: `hsl(${this.state.hue}, ${this.state.saturation}%, ${
-            this.state.lightness
-          }%)`
-        }}
-      >
-        <h1>Color Slider</h1>
-        <div className="slide-container">
-          <section>
-            <h2>Hue is {this.state.hue}</h2>
-            <input
-              type="range"
-              min="1"
-              max="360"
-              className="slider"
-              id="Hue"
-              onChange={this.updateHue}
-            />
-          </section>
-          <section>
-            <h3>Saturation is {this.state.saturation}%</h3>
-            <input
-              type="range"
-              min="1"
-              max="100"
-              className="slider"
-              id="Saturation"
-              onChange={this.updateSaturation}
-            />
-          </section>
-          <section>
-            <h4>Lightness is {this.state.lightness}%</h4>
-            <input
-              type="range"
-              min="1"
-              max="100"
-              className="slider"
-              id="Lightness"
-              onChange={this.updateLightness}
-            />
-          </section>
-          <button className="reset-button" onClick={this.updateColor}>
-            reset color
-          </button>
-        </div>
-      </main>
-    )
-  }
+  return (
+    <main
+      style={{
+        backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}%)`
+      }}
+    >
+      <h1>Color Slider</h1>
+      <div className="slide-container">
+        <section>
+          <h2>Hue is {hue}</h2>
+          <input
+            type="range"
+            min="1"
+            max="360"
+            className="slider"
+            id="Hue"
+            onChange={updateHue}
+          />
+        </section>
+        <section>
+          <h3>Saturation is {saturation}%</h3>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            className="slider"
+            id="Saturation"
+            onChange={updateSaturation}
+          />
+        </section>
+        <section>
+          <h4>Lightness is {lightness}%</h4>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            className="slider"
+            id="Lightness"
+            onChange={updateLightness}
+          />
+        </section>
+        <button className="reset-button" onClick={updateColor}>
+          reset color
+        </button>
+      </div>
+    </main>
+  )
 }
-
-export default ColorSliderMain
